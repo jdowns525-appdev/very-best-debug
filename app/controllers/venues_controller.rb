3405962126be5d358a9845eb27c578eb4007a067
jdowns525 <downsjosseph@gmail.com>
2023-03-13 16:40:34 +0000

    added venues details page

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -2,25 +2,32 @@ class VenuesController < ApplicationController
 
   def index
     matching_venues = Venue.all
-    @venues = matching_venues.order(:created_at)
+    @list_of_venues = matching_venues.order({:created_at => :asc})
 
     render({ :template => "venue_templates/venue_list.html.erb" })
   end
 
   def show
-    venue_id = params.fetch("venue_id")
+    venue_id = params.fetch("an_id")
     matching_venues = Venue.where({ :id => venue_id })
-    the_venue = matching_venues
+    @venue = matching_venues.at(0)
 
     render({ :template => "venue_templates/details.html.erb" })
   end
 
   def create
-    @venue = Venue.new
-    @venue.address = params.fetch("query_address")
-    @venue.name = params.fetch("query_name")
-    @venue.neighborhood = params.fetch("query_neighborhood")
-    @venue.save
+
+    input_address = params.fetch("query_address")
+    input_name = params.fetch("query_name")
+    input_neighborhood = params.fetch("query_neighborhood")
+
+
+    new_venue = Venue.new
+    new_venue.address = input_address
+    new_venue.name = input_name
+    new_venue.neighborhood = input_neighborhood
+
+    new_venue.save
 
     redirect_to("/venues/#{venue.name}")
   end
@@ -29,10 +36,10 @@ class VenuesController < ApplicationController
     the_id = params.fetch("venue_id")
 
     @venue = Venue.where({ :id => the_id })
-    venue.address = params.fetch("query_address")
-    venue.name = params.fetch("Query_name")
-    venue.neighborhood = params.fetch("query_neighborhood")
-    venue.save
+    @venue.address = params.fetch("query_address")
+    @venue.name = params.fetch("Query_name")
+    @venue.neighborhood = params.fetch("query_neighborhood")
+    @venue.save
     
     redirect_to("/venues/#{venue.id}")
   end
@@ -40,7 +47,7 @@ class VenuesController < ApplicationController
   def destroy
     the_id = params.fetch("venue_id")
     matching_venues = Venue.where({ :id => the_id })
-    venue = matching_venues
+    venue = matching_venues.at(0)
     venue.destroy
 
     redirect_to("/venues")
